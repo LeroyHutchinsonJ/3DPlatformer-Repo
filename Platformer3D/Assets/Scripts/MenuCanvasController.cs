77731963fcd7904638530a93971371bea71926ba
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2020-04-20 15:46:56 -0400

    Update Menu
    Updated the menu button, Added two button and connected functions to them

---
+++ Platformer3D/Assets/Scripts/MenuCanvasController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCanvasController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //This function is gonna activate when the play button is pressed
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //This function is gonna activate when the quit button is pressed
+    public void OnQuitButton()
+    {
+        //This will quit out the application
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
